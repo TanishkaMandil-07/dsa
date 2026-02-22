a96eeb4c7b70eefe06889a8ad09b9cbf767ad5fc
TanishkaMandil-07 <tanishkamandil4@gmail.com>
2026-02-22 22:41:57 +0530

    Create kthsmallestelement.cpp

---
+++ Arrayquestion/kthsmallestelement.cpp
@@ -0,0 +1,11 @@
+// Find the Kth Smallest Element]
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int>a={7,10,4,3,20,15};
+    int k=3;
+
+    sort(a.begin(),a.end());
+    cout<<a[k-1];
+}
